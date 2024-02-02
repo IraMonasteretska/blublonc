e32fc8ae9de8c6425f862b252ab0b64bee49149d
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2024-02-02 16:14:51 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -25,29 +25,58 @@ window.addEventListener('DOMContentLoaded', function () {
 
 
 $(document).ready(function() {
+
     $('.tablewrap').each(function() {
         var $tablewrap = $(this);
         var $totaltd = $tablewrap.find('.totaltd');
         var $warningText = $tablewrap.find('.warningtext');
         var $inputs = $tablewrap.find('input[type="number"]');
-
+        var minTotal = parseInt($tablewrap.data('min')); 
+    
         function checkTotal() {
             var sum = 0;
             $inputs.each(function() {
                 sum += parseInt($(this).val()) || 0; 
             });
-
-            if (sum < 8) {
+    
+            if (sum < minTotal) { 
                 $warningText.show();
             } else {
                 $warningText.hide();
             }
         }
-
+    
         checkTotal();
         $inputs.on('input', function() {
             checkTotal();
         });
     });
+
+    
+    $('.btform').submit(function(event) {
+        event.preventDefault(); 
+       
+        var $tablesWithWarnings = $('.tablewrap').filter(function() {
+            var $tablewrap = $(this);
+            var sum = 0;
+            $tablewrap.find('input[type="number"]').each(function() {
+                sum += parseInt($(this).val()) || 0;
+            });
+            var minTotal = parseInt($tablewrap.data('min'));
+            return sum < minTotal;
+        });
+    
+        if ($tablesWithWarnings.length > 0) {
+            
+            var offset = $tablesWithWarnings.first().offset().top;
+            $('html, body').animate({
+                scrollTop: offset
+            }, 500); 
+        } else {
+            this.submit();
+        }
+    });
+
+
 });
 
